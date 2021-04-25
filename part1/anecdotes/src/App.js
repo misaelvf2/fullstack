3ec970cd729073620b372d81011edb2268a8d594
Misael Valentin <misael.vf2@gmail.com>
2021-04-24 23:13:31 -0400

    Fixed state

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,45 +1,50 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code acounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+const Anecdote = ({ text, votes=0 }) => {
+  return (
+    <div>
+      <div>
+        {text}
+      </div>
+      <div>
+        has {votes} votes
+      </div>
+    </div>
+  )
+}
 
-  const handleNext = () => {
-    const randomNumber = Math.floor(Math.random() * anecdotes.length)
+const App = (props) => {
+  const next = () => {
+    let randomNumber = Math.floor(Math.random() * props.anecdotes.length)
+    while (randomNumber === selected) {
+      randomNumber = Math.floor(Math.random() * props.anecdotes.length)
+    }
     setSelected(randomNumber)
-    console.log(randomNumber)
   }
 
-  const handleVote = (selected) => {
-    const newVotes = [ ...votes ]
+  const vote = () => {
+    const newVotes = [...votes]
     newVotes[selected] += 1
-    setVotes(newVotes)
-  }
 
-  const getMostVotes = () => {
-    const maxIndex = votes.indexOf(Math.max(...votes))
-    return maxIndex
+    setVotes(newVotes)
+    if (votes[selected] + 1 > votes[mostVotes]) {
+      setMostVotes(selected)
+    }
   }
 
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [votes, setVotes] = useState(Array(props.anecdotes.length).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      {anecdotes[selected]}
-      <br/>has {votes[selected]} votes
-      <br/><button onClick={() => handleVote(selected)}>vote</button>
-      <button onClick={handleNext}>next anecdote</button>
+      <h2>Anecdote of the day</h2>
+      <Anecdote text={props.anecdotes[selected]} votes={votes[selected]} />
+      <button onClick={vote}>vote</button>
+      <button onClick={next}>next anecdote</button>
 
-      <h1>Anecdote with most votes</h1>
-      {anecdotes[getMostVotes()]}
+      <h2>Anecdote with most votes</h2>
+      <Anecdote text={props.anecdotes[mostVotes]} votes={votes[mostVotes]} />
     </div>
   )
 }
