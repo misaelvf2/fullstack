15452938b71fb25714d826777bd561ed368bbbce
Misael Valentin <misael.vf2@gmail.com>
2021-05-02 18:36:53 -0400

    Exercise 2.13

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const CountrySearch = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        find countries
+        <input search={props.search} onChange={props.handleSearchChange} />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+const Country = (props) => {
+  return (
+    <div>
+      <h2>{props.country.name}</h2>
+      <div>capital {props.country.capital}</div>
+      <div>population {props.country.population}</div>
+
+      <h3>languages</h3>
+      <ul>
+        {props.country.languages.map(language =>
+          <li key={language.name}>{language.name}</li>
+        )}
+      </ul>
+      <img src={props.country.flag} height="100px" weight="100px" alt="Flag" />
+    </div>
+  )
+}
+
+const SearchResults = ({ countriesToShow }) => {
+  if (countriesToShow.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  } else if (countriesToShow.length > 1 && countriesToShow.length <= 10) {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <div key={country.name}>{country.name}</div>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <Country key={country.name} country={country} />
+        )}
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [countriesToShow, setCountriesToShow] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const searchCountry = (event) => {
+    event.preventDefault()
+    setCountriesToShow(countries.filter(country => country.name.toLowerCase().indexOf(search.toLocaleLowerCase()) !== -1))
+  }
+
+  return (
+    <div>
+      <CountrySearch handleSubmit={searchCountry} search={search} handleSearchChange={handleSearchChange} />
+      <SearchResults countriesToShow={countriesToShow} />
+    </div>
+  )
+}
+
+export default App
