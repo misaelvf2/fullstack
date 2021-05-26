38b634342333de46e3a75c2bda93758577b8cca6
Misael Valentin <misael.vf2@gmail.com>
2021-05-26 03:33:02 -0400

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,7 +36,12 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
-  response.json(person)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
